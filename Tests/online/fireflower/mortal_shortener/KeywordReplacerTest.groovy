92f2a6186193b00ac36e74b9728dc0b3b5a82134
Exeton <benjamin.diederich@gmail.com>
2018-10-13 16:40:15 -0400

    Added unit tests

---
+++ Tests/online/fireflower/mortal_shortener/KeywordReplacerTest.groovy
@@ -0,0 +1,33 @@
+package online.fireflower.mortal_shortener
+
+import org.junit.Assert
+import org.junit.Test
+
+class KeywordReplacerTest extends GroovyTestCase {
+
+    @Test
+    void testReplace() throws Exception{
+
+        HashMap<String, String> replacements = new HashMap<>();
+        replacements.put("#hacks", "for hacking");
+        KeywordReplacer keywordReplacer = new KeywordReplacer(replacements);
+
+
+        String message = "/ban Notch #hacks";
+        String expected = "/ban Notch for hacking";
+        Assert.assertEquals(expected, keywordReplacer.replace(message))
+    }
+
+    @Test
+    void testReplacementSubstrings() throws Exception{
+
+        HashMap<String, String> replacements = new HashMap<>();
+        replacements.put("#reason", "for cheating");
+        replacements.put("#reasonA", "for being a big noob");//This one must follow "reasons"
+        KeywordReplacer keywordReplacer = new KeywordReplacer(replacements);
+
+
+        String message = "/ban Notch #reasonA";
+        Assert.assertEquals("/ban Notch for being a big noob", keywordReplacer.replace(message));
+    }
+}
